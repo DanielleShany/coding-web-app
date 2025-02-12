01ab0d9d80518a8952dcbe9255405d128b494db1
Danielle Shany <daniellesh2@mta.ac.il>
2025-02-12 18:13:10 +0200

    didnt change socket

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,12 +1,12 @@
-// // client/src/socket.ts
-// import { io, Socket } from 'socket.io-client';
+// client/src/socket.ts
+import { io, Socket } from 'socket.io-client';
 
-// const socket: Socket = io('http://localhost:4000', { transports: ['websocket'] });
+const socket: Socket = io('http://localhost:4000', { transports: ['websocket'] });
 
-// socket.on('connect', () => {
-//     console.log(`✅ Connected to server with socket ID: ${socket.id}`);
-//   });
+socket.on('connect', () => {
+    console.log(`✅ Connected to server with socket ID: ${socket.id}`);
+  });
 
-// export default socket;
+export default socket;
 
 
